b857b4db376babf736da7414ada24a849e90e431
githubMay <38322350+githubMay@users.noreply.github.com>
2018-06-17 19:23:23 +0800

    Update main.py

--- hw02/main.py
+++ hw02/main.py
@@ -1,71 +1,47 @@
-
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <script type="text/javascript">
+        function getValue() {
+
+            var inputNum = document.getElementById("d1").value;
+            var d2 = document.getElementById("d2");
+            if (inputNum > 0 && inputNum < 10) {
+                var tab = "<table border='1' >";
+
+                for (var i = 1; i <= inputNum; i++) {
+                tab += "<tr>";
+
+                for (var j = 1; j <= inputNum; j++) {
+                    tab +=
+                        "<td style='background-color:rgb(86, 220, 30);width:80px;height:80px;margin:10px;'>"
+                    if (j <= i) {
+                        tab += j + "×" + i + "=" + i * j;
+                        tab += "</td>";
+                    };
+
+
+                }
+                tab += "</tr>";
+            }
+                tab += "</table>"
+                d2.innerHTML = tab;
+            } else alert("输入错误！请输入数字1 - 9");
+
+        }
+    </script>
+</head>
 
 <body>
-    <div>
-        <table width="611.2" border="0">
-            <tr>
-                <td colspan="3" style="background-color:#99bbbb;">
-                    <h1>Main Title of Web Page</h1>
-                </td>
-            </tr>
 
-            <tr>
-                <td>
-                    <table style="margin:0px;">
-                        <tr>
-                            <td style="background-color:#ffff99;width:200px;text-align:top;">
-                                <b>Menu</b>
-                                <br /> HTML
-                                <br /> CSS
-                                <br /> JavaScript
-                            </td>   
-                            <td style="background-color:#EEEEEE;height:100px;width:200px;text-align:top;">
-                                Content goes here</td>
-                            <td style="background-color:rgb(146, 138, 219);height:100px;width:200px;text-align:top;">
-                                Content goes here</td>
-
-                        </tr>
-                        <tr>
-                            <td style="background-color:#ffff99;width:200px;text-align:top;">
-                                <b>Menu</b>
-                                <br /> HTML
-                                <br /> CSS
-                                <br /> JavaScript
-                            </td>
-                            <td style="background-color:#EEEEEE;height:100px;width:200px;text-align:top;">
-                                Content goes here</td>
-                            <td style="background-color:rgb(146, 138, 219);height:100px;width:200px;text-align:top;">
-                                Content goes here</td>
-                            <tr>
-                                <td style="background-color:#ffff99;width:200px;text-align:top;">
-                                    <b>Menu</b>
-                                    <br /> HTML
-                                    <br /> CSS
-                                    <br /> JavaScript
-                                </td>
-                                <td style="background-color:#EEEEEE;height:100px;width:200px;text-align:top;">
-                                    Content goes here</td>
-                                <td style="background-color:rgb(146, 138, 219);height:100px;width:200px;text-align:top;">
-                                    Content goes here</td>
-
-                            </tr>
-
-
-                        </tr>
-
-                    </table>
-                </td>
-            </tr>
-
-            <tr>
-                <td colspan="2" style="background-color:#99bbbb;text-align:center;">
-                    Copyright W3School.com.cn</td>
-            </tr>
-    
-        </table>
-    </div>
+    <input type="text" id="d1" placeholder="请输入数字1-9">
+    <input type="button" value="点击生成表格" onclick="getValue()">
+    <div id="d2"></div>
 
 </body>
 
